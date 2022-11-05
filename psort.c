f6bc764241e55a4e061afae09540bf687436e955
TanishaHegde98 <71375974+TanishaHegde98@users.noreply.github.com>
2022-11-05 17:50:00 -0500

    Create template for psort

---
+++ psort.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#incldue <sys/stat.h>
+#include <sys/mman.h>
